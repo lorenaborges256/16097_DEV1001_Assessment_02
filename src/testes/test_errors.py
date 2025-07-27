c4269a8acb71b625f81cf476d7841062e082977a
lorenaborges256 <lorena.borges.amaral@gmail.com>
2025-07-27 13:58:18 +1000

    fix: added empty __init__.py file on src folder to fix ModuleNotFoundError

--- src/testes/test_errors.py
+++
@@ -1,8 +0,0 @@
-# import pytest
-# from user_auth import is_valid_email
-
-# def test_is_valid_email(email):
-#     assert is_valid_email(email) is not None, f"Email validation failed for: {email}"
-
-def test_is_valid_email_seccess():
-    pass
